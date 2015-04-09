7dc0d521659bc901385463a55b198947775c4396
timothywong24 <timothywong7@gmail.com>
2015-04-09 19:06:52 -0400

    9 April 2015 oop results

--- public/js/app.js
+++ public/js/app.js
@@ -30,10 +30,8 @@ app.controller('profileController', function($scope, $http) {
     $scope.personalResults[2] = {type:'Contest Personal Records'};
     $scope.personalResults[3] = {type:'Current Week Results'};
 
-    $scope.personalResults[0].results = [];
-    $scope.personalResults[1].results = [];
-    $scope.personalResults[2].results = [];
-    $scope.personalResults[3].results = [];
+    for (var i = 0; i < 3; i++)
+        $scope.personalResults[i].results = [];
 
     $scope.personalResults[0].results[0] = {event:"Rubik's Cube", single:'5.79', average:'8.50'};
     $scope.personalResults[0].results[1] = {event:"4x4 Cube", single:'27.34', average:'32.51'};
@@ -47,9 +45,34 @@ app.controller('profileController', function($scope, $http) {
 
 app.controller('resultsController', function($scope, $http) {
 
-    $scope.events = ["Rubik's Cube", "4x4 Cube", "5x5 Cube", "2x2 Cube", "3x3 Blindfolded", "3x3 One-Handed", "Pyraminx"];
-    $scope.selectedEvent = "Rubik's Cube";
+    $scope.events = [];
+    $scope.events[0] = {id:0, name:"Rubik's Cube"};
+    $scope.events[1] = {id:1, name:"4x4 Cube"};
+    $scope.events[2] = {id:2, name:"5x5 Cube"};
+    $scope.events[3] = {id:3, name:"2x2 Cube"};
+    $scope.events[4] = {id:4, name:"3x3 Blindfolded"};
+    $scope.events[5] = {id:5, name:"3x3 One-Handed"};
+    $scope.events[6] = {id:6, name:"Pyraminx"};
 
+    for (var i = 0; i < 7; i++)
+        $scope.events[i].results = {};
+
+    $scope.events[0].results[0] = {name:'Tim Wong', result:'10.40'};
+    $scope.events[1].results[0] = {name:'Tim Wong', result:'38.80'};
+    $scope.events[2].results[0] = {name:'Tim Wong', result:'1:23.47'};
+    $scope.events[3].results[0] = {name:'Tim Wong', result:'4.11'};
+    $scope.events[4].results[0] = {name:'Tim Wong', result:'32.44'};
+    $scope.events[5].results[0] = {name:'Tim Wong', result:'17.44'};
+    $scope.events[6].results[0] = {name:'Tim Wong', result:'7.14'};
+
+    $scope.events[0].results[1] = {name:'Edward Lin', result:'8.80'};
+    $scope.events[1].results[1] = {name:'Edward Lin', result:'36.25'};
+    $scope.events[2].results[1] = {name:'Edward Lin', result:'1:12.41'};
+    $scope.events[3].results[1] = {name:'Edward Lin', result:'2.48'};
+    $scope.events[5].results[1] = {name:'Edward Lin', result:'15.21'};
+    $scope.events[6].results[1] = {name:'Edward Lin', result:'6.66'};
+
+    $scope.selectedEvent = $scope.events[0];
     $scope.selectEvent = function(event) {
         $scope.selectedEvent = event;
     };
