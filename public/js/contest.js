4fc75e355cd0dc93ca95dbf6ec737913d12e5382
timothywong24 <timothywong7@gmail.com>
2015-04-18 16:14:11 -0400

    18 April 2015 1614 fixed enter key default

--- public/js/contest.js
+++ public/js/contest.js
@@ -222,6 +222,14 @@ function reformatTime(time) {
     }
 }
 
+function stopRKey(evt) {
+    var evt = (evt) ? evt : ((event) ? event : null);
+    var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+    if ((evt.keyCode == 13) && (node.type=="text"))  {return false;}
+}
+
+document.onkeypress = stopRKey;
+
 window.fbAsyncInit = function() {
     FB.init({
         appId: '1397096627278092',
