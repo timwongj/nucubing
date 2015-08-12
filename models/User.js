4cc1fb085fd8263b0ec07c455db6725db86e1b8b
timwongj <wong.t@husky.neu.edu>
2015-08-11 20:39:50 -0400

    exported mongoose schemas

---
+++ models/User.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ObjectId = Schema.ObjectId;
+
+var UserSchema = new mongoose.Schema({
+  id:ObjectId,
+  facebook_id:String,
+  firstName: String,
+  lastName: String,
+  email:String,
+  wcaID:String,
+  provider:String
+});
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = User;
