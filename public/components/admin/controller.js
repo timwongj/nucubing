9043a336aaacdae50a6c072d7514edb89a8f3a84
timwongj <wong.t@husky.neu.edu>
2015-08-14 10:57:44 -0400

    fixed minor bugs and finished admin page

--- public/components/admin/controller.js
+++ public/components/admin/controller.js
@@ -13,9 +13,15 @@
         $scope.authStatus = 'Login';
     });
 
+    $scope.weeks = getAllScrambles($http);
+
     $scope.uploader = new FileUploader({
-      url:'/admin/scrambles/upload'
+      url:'/admin/scrambles'
     });
+
+    $scope.uploader.onSuccessItem = function() {
+      $scope.weeks = getAllScrambles($http);
+    }
 
   }
 
@@ -24,6 +30,31 @@
   angular.module('nuCubingApp').controller('AdminController', AdminController);
 
 })();
+
+function getAllScrambles($http) {
+  var weeks = [];
+  $http.get('/admin/scrambles').success(function(response) {
+    for (var i = 0; i < response.length; i++) {
+      var index = -1;
+      for (var j = 0; j < weeks.length; j++) {
+        if (weeks[j].week == response[i].week) {
+          index = j;
+          break;
+        }
+      }
+      if (index < 0) {
+        weeks.push({week: response[i].week, events: []});
+        index = weeks.length - 1;
+      }
+      weeks[index].events.push({
+        event: response[i].event,
+        scrambles: response[i].scrambles,
+        extraScrambles: response[i].extraScrambles
+      });
+    }
+  });
+  return weeks;
+}
 
 // facebook
 window.fbAsyncInit = function() {
