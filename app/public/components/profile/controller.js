8debb3f9b1952794740a03f7096c6f61d2792893
timwongj <wong.t@husky.neu.edu>
2015-08-18 00:55:18 -0400

    exported calculator functions to angular service

--- app/public/components/profile/controller.js
+++ app/public/components/profile/controller.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  function ProfileController($scope, $http, $location) {
+  function ProfileController($scope, $http, $location, Calculator) {
 
     // get authorization status
     $http.get('/auth/status').success(function(response) {
@@ -77,20 +77,20 @@
         data = JSON.parse(results[i].data);
         switch($scope.personalBestsMap[results[i].event].format) {
           case 'avg5':
-            $scope.personalBestsMap[results[i].event].single = compareResults($scope.personalBestsMap[results[i].event].single, calculateSingle(data.times, data.penalties));
-            $scope.personalBestsMap[results[i].event].average = compareResults($scope.personalBestsMap[results[i].event].average, calculateAverage(data.times, data.penalties));
+            $scope.personalBestsMap[results[i].event].single = Calculator.compareResults($scope.personalBestsMap[results[i].event].single, Calculator.calculateSingle(data.times, data.penalties));
+            $scope.personalBestsMap[results[i].event].average = Calculator.compareResults($scope.personalBestsMap[results[i].event].average, Calculator.calculateAverage(data.times, data.penalties));
             break;
           case 'mo3':
           case 'bo3':
-            $scope.personalBestsMap[results[i].event].single = compareResults($scope.personalBestsMap[results[i].event].single, calculateSingle(data.times, data.penalties));
-            $scope.personalBestsMap[results[i].event].average = ((results[i].event == '555bf') || (results[i].event == '444bf')) ? '' : compareResults($scope.personalBestsMap[results[i].event].average, calculateMean(data.times, data.penalties));
+            $scope.personalBestsMap[results[i].event].single = Calculator.compareResults($scope.personalBestsMap[results[i].event].single, Calculator.calculateSingle(data.times, data.penalties));
+            $scope.personalBestsMap[results[i].event].average = ((results[i].event == '555bf') || (results[i].event == '444bf')) ? '' : Calculator.compareResults($scope.personalBestsMap[results[i].event].average, Calculator.calculateMean(data.times, data.penalties));
             break;
           case 'fmc':
-            $scope.personalBestsMap[results[i].event].single = compareResults($scope.personalBestsMap[results[i].event].single, calculateFMCSingle(data.moves));
-            $scope.personalBestsMap[results[i].event].average = compareResults($scope.personalBestsMap[results[i].event].average, calculateFMCMean(data.moves));
+            $scope.personalBestsMap[results[i].event].single = Calculator.compareResults($scope.personalBestsMap[results[i].event].single, Calculator.calculateFMCSingle(data.moves));
+            $scope.personalBestsMap[results[i].event].average = Calculator.compareResults($scope.personalBestsMap[results[i].event].average, Calculator.calculateFMCMean(data.moves));
             break;
           case 'mbld':
-            var mbldResult = compareMBLDResults($scope.personalBestsMap[results[i].event].single, data);
+            var mbldResult = Calculator.compareMBLDResults($scope.personalBestsMap[results[i].event].single, data);
             $scope.personalBestsMap[results[i].event].single = mbldResult.solved + '/' + mbldResult.attempted + ' in ' + mbldResult.time;
             break;
         }
@@ -120,27 +120,27 @@
         var result = {'event':$scope.eventMap[results[i].event].name, 'week':results[i].week, 'index':$scope.eventMap[results[i].event].index};
         switch($scope.eventMap[results[i].event].format) {
           case 'avg5':
-            result.best = calculateSingle(data.times, data.penalties);
-            result.average = calculateAverage(data.times, data.penalties);
+            result.best = Calculator.calculateSingle(data.times, data.penalties);
+            result.average = Calculator.calculateAverage(data.times, data.penalties);
             result.details = '';
-            formattedTimes = formatTimes(data.times, data.penalties);
+            formattedTimes = Calculator.formatTimes(data.times, data.penalties);
             for (j = 0; j < data.times.length; j++) {
-              result.details += (j == data.times.length - 1) ? reformatTime(formattedTimes[j]) : reformatTime(formattedTimes[j]) + ', ';
+              result.details += (j == data.times.length - 1) ? Calculator.reformatTime(formattedTimes[j]) : Calculator.reformatTime(formattedTimes[j]) + ', ';
             }
             break;
           case 'mo3':
           case 'bo3':
-            result.best = calculateSingle(data.times, data.penalties);
-            result.average = ((results[i].event == '555bf') || (results[i].event == '444bf')) ? '' : calculateMean(data.times, data.penalties);
+            result.best = Calculator.calculateSingle(data.times, data.penalties);
+            result.average = ((results[i].event == '555bf') || (results[i].event == '444bf')) ? '' : Calculator.calculateMean(data.times, data.penalties);
             result.details = '';
-            formattedTimes = formatTimes(data.times, data.penalties);
+            formattedTimes = Calculator.formatTimes(data.times, data.penalties);
             for (j = 0; j < data.times.length; j++) {
-              result.details += (j == data.times.length - 1) ? reformatTime(formattedTimes[j]) : reformatTime(formattedTimes[j]) + ', ';
+              result.details += (j == data.times.length - 1) ? Calculator.reformatTime(formattedTimes[j]) : Calculator.reformatTime(formattedTimes[j]) + ', ';
             }
             break;
           case 'fmc':
-            result.best = calculateFMCSingle(data.moves);
-            result.average = calculateFMCMean(data.moves);
+            result.best = Calculator.calculateFMCSingle(data.moves);
+            result.average = Calculator.calculateFMCMean(data.moves);
             result.details = data.moves[0] + ', ' + data.moves[1] + ', ' + data.moves[2];
             break;
           case 'mbld':
@@ -160,8 +160,11 @@
 
   }
 
-  function OrderObjectByFilter() {
+
+  function orderObjectBy() {
+
     return function(items, field) {
+
       var filtered = [];
       angular.forEach(items, function(item, key) {
         item.key = key;
@@ -171,216 +174,257 @@
         return (a[field] > b[field] ? 1 : -1);
       });
       return filtered;
+
     };
+
+  }
+
+  function Calculator() {
+
+    return {
+
+      /**
+       * Compares two results
+       * @param res1
+       * @param res2
+       * @returns {*}
+       */
+      compareResults: function(res1, res2) {
+        if (!res1) {
+          return res2;
+        } else if (res1 == 'DNF') {
+          return (res2 == 'DNF') ? '' : res2;
+        } else if (res2 == 'DNF') {
+          return res1;
+        } else {
+          var res1Arr = res1.toString().split(':'), res2Arr = res2.toString().split(':'), res1Formatted, res2Formatted;
+          res1Formatted = (res1Arr.length > 1) ? (parseFloat(res1Arr[0]) * 60) + parseFloat(res1Arr[1]) : parseFloat(res1Arr[0]);
+          res2Formatted = (res2Arr.length > 2) ? (parseFloat(res2Arr[0]) * 60) + parseFloat(res2Arr[2]) : parseFloat(res2Arr[0]);
+          return (res1Formatted > res2Formatted) ? res2 : res1;
+        }
+      },
+
+      /**
+       * Compares two MBLD results
+       * @param res1
+       * @param res2
+       * @returns {*}
+       */
+      compareMBLDResults: function(res1, res2) {
+        if (!res1) {
+          return res2;
+        }
+        var score1 = res1.solved - (res1.attempted - res1.solved), score2 = res2.solved - (res2.attempted - res2.solved);
+        if (score1 > score2) {
+          return res1;
+        } else if (score1 < score2) {
+          return res2;
+        } else if (res1.time < res2.time) {
+          return res1;
+        } else if (res1.time > res2.time) {
+          return res2;
+        } else if (res1.attempted < res2.attempted) {
+          return res1;
+        } else {
+          return res2;
+        }
+      },
+
+      /**
+       * Calculates FMC Single
+       * @param moves
+       * @returns {string}
+       */
+      calculateFMCSingle: function(moves) {
+        var i, single = 'DNF';
+        for (i = 0; i < moves.length; i++) {
+          if (moves[i] != 'DNF') {
+            if (single == 'DNF') {
+              single = moves[i];
+            }
+            if (parseFloat(moves[i]) < single) {
+              single = moves[i];
+            }
+          }
+        }
+        return single;
+      },
+
+      /**
+       * Calculates the best single time
+       * @param times
+       * @param penalties
+       * @returns {*}
+       */
+      calculateSingle: function(times, penalties) {
+        var i, single = 'DNF', formattedTimes = this.formatTimes(times, penalties);
+        for (i = 0; i < formattedTimes.length; i++) {
+          if (formattedTimes[i] != 'DNF') {
+            if (single == 'DNF') {
+              single = formattedTimes[i];
+            } else if (parseFloat(formattedTimes[i]) < single) {
+              single = formattedTimes[i];
+            }
+          }
+        }
+        if (single == 'DNF') {
+          return single;
+        } else {
+          return this.reformatTime(single);
+        }
+      },
+
+      /**
+       * Calculate the trimmed average of 5 given the array of times and penalties
+       * @param times
+       * @param penalties
+       * @returns {*}
+       */
+      calculateAverage: function(times, penalties) {
+        var formattedTimes = this.formatTimes(times, penalties);
+        var i, DNFCount = 0, minIndex, maxIndex, minValue, maxValue;
+        for (i = 0; i < formattedTimes.length; i++) {
+          if (formattedTimes[i] == 'DNF') {
+            DNFCount++;
+          }
+        }
+        if (DNFCount > 1) {
+          return 'DNF';
+        }
+        if (formattedTimes[0] != 'DNF') {
+          minIndex = 0;
+          maxIndex = 0;
+          minValue = parseFloat(formattedTimes[0]);
+          maxValue = parseFloat(formattedTimes[0]);
+        } else {
+          minIndex = 1;
+          maxIndex = 1;
+          minValue = parseFloat(formattedTimes[1]);
+          maxValue = parseFloat(formattedTimes[1]);
+        }
+        for (i = 0; i < formattedTimes.length; i++) {
+          if (formattedTimes[i] == 'DNF') {
+            maxValue = formattedTimes[i];
+            maxIndex = i;
+            break;
+          }
+          if (parseFloat(formattedTimes[i]) > maxValue) {
+            maxValue = parseFloat(formattedTimes[i]);
+            maxIndex = i;
+          }
+        }
+        for (i = 0; i < formattedTimes.length; i++) {
+          if ((i != maxIndex) && (parseFloat(formattedTimes[i]) < minValue)) {
+            minValue = parseFloat(formattedTimes[i]);
+            minIndex = i;
+          }
+        }
+        if ((minIndex === 0) && (maxIndex === 0)) {
+          maxIndex = 1;
+        }
+        var sum = 0;
+        for (i = 0; i < formattedTimes.length; i++) {
+          if ((i != minIndex) && (i != maxIndex)) {
+            sum += parseFloat(formattedTimes[i]);
+          }
+        }
+        var average = sum / (formattedTimes.length - 2);
+        return this.reformatTime(average);
+      },
+
+      /**
+       * Calculate FMC mean of 3
+       * @param moves
+       * @returns {string}
+       */
+      calculateFMCMean: function(moves) {
+        return ((moves[0] + moves[1] + moves[2]) / 3).toFixed(2);
+      },
+
+      /**
+       * Calculate the mean of 3 given the array of times and penalties
+       * @param times
+       * @param penalties
+       * @returns {*}
+       */
+      calculateMean: function(times, penalties) {
+        var formattedTimes = this.formatTimes(times, penalties);
+        var i, DNFCount = 0;
+        for (i = 0; i < formattedTimes.length; i++) {
+          if (formattedTimes[i] == 'DNF') {
+            DNFCount++;
+          }
+        }
+        if (DNFCount > 0) {
+          return 'DNF';
+        }
+        var sum = 0;
+        for (i = 0; i < formattedTimes.length; i++) {
+          sum += parseFloat(formattedTimes[i]);
+        }
+        var mean = sum / formattedTimes.length;
+        return this.reformatTime(mean);
+      },
+
+      /**
+       * return an array of results in milliseconds taking penalties into account
+       * @param times
+       * @param penalties
+       * @returns {Array}
+       */
+      formatTimes: function(times, penalties) {
+        var formattedTimes = [];
+        var i, unsplitTimes = [];
+        for (i = 0; i < times.length; i++) {
+          var res = times[i].split(':');
+          if (res.length > 1) {
+            unsplitTimes[i] = (parseFloat(res[0]) * 60) + parseFloat(res[1]);
+          } else {
+            unsplitTimes[i] = parseFloat(res[0]);
+          }
+          if ((penalties[i] == '(DNF)') || (unsplitTimes[i] === '')) {
+            formattedTimes[i] = 'DNF';
+          } else if (penalties[i] == '(+2)') {
+            formattedTimes[i] = parseFloat(unsplitTimes[i]) + 2;
+          } else {
+            formattedTimes[i] = unsplitTimes[i];
+          }
+        }
+        return formattedTimes;
+      },
+
+      /**
+       * Converts the time from milliseconds to minutes:seconds.milliseconds
+       * @param time
+       * @returns {*}
+       */
+      reformatTime: function(time) {
+        if (isNaN(time)) {
+          return 'DNF';
+        }
+        if (parseFloat(time) <  60) {
+          return parseFloat(time).toFixed(2);
+        } else {
+          var min = Math.floor(parseFloat(time) / 60);
+          var sec = (parseFloat(time) % 60).toFixed(2);
+          if (sec < 10) {
+            return min + ':0' + sec;
+          } else {
+            return min + ':' + sec;
+          }
+        }
+      }
+
+    };
+
   }
 
   angular.module('nuCubingApp', ['ui.bootstrap']);
 
-  angular.module('nuCubingApp').filter('orderObjectBy', OrderObjectByFilter);
+  angular.module('nuCubingApp').filter('orderObjectBy', orderObjectBy);
+
+  angular.module('nuCubingApp').factory('Calculator', Calculator);
 
   angular.module('nuCubingApp').controller('ProfileController', ProfileController);
 
 })();
-
-// compare two results
-function compareResults(res1, res2) {
-  if (!res1) {
-    return res2;
-  } else if (res1 == 'DNF') {
-    if (res2 == 'DNF') {
-      return '';
-    } else {
-      return res2;
-    }
-  } else if (res2 == 'DNF') {
-    return res1;
-  } else {
-    var res1Arr = res1.toString().split(':'), res2Arr = res2.toString().split(':'), res1Formatted, res2Formatted;
-    if (res1Arr.length > 1) {
-      res1Formatted = (parseFloat(res1Arr[0]) * 60) + parseFloat(res1Arr[1]);
-    } else {
-      res1Formatted = parseFloat(res1Arr[0]);
-    }
-    if (res2Arr.length > 2) {
-      res2Formatted = (parseFloat(res2Arr[0]) * 60) + parseFloat(res2Arr[2]);
-    } else {
-      res2Formatted = parseFloat(res2Arr[0]);
-    }
-    if (res1Formatted > res2Formatted) {
-      return res2;
-    } else {
-      return res1;
-    }
-  }
-}
-
-function compareMBLDResults(res1, res2) {
-  if (!res1) {
-    return res2;
-  }
-  var score1 = res1.solved - (res1.attempted - res1.solved), score2 = res2.solved - (res2.attempted - res2.solved);
-  if (score1 > score2) {
-    return res1;
-  } else if (score1 < score2) {
-    return res2;
-  } else if (res1.time < res2.time) {
-    return res1;
-  } else if (res1.time > res2.time) {
-    return res2;
-  } else if (res1.attempted < res2.attempted) {
-    return res1;
-  } else {
-    return res2;
-  }
-}
-
-// calculate the best fmc single
-function calculateFMCSingle(moves) {
-  var i, single = 'DNF';
-  for (i = 0; i < moves.length; i++) {
-    if (moves[i] != 'DNF') {
-      if (single == 'DNF')
-        single = moves[i];
-      if (parseFloat(moves[i]) < single)
-        single = moves[i];
-    }
-  }
-  return single;
-}
-
-// calculate the best single time
-function calculateSingle(times, penalties) {
-  var i, single = 'DNF', formattedTimes = formatTimes(times, penalties);
-  for (i = 0; i < formattedTimes.length; i++) {
-    if (formattedTimes[i] != 'DNF') {
-      if (single == 'DNF') {
-        single = formattedTimes[i];
-      } else if (parseFloat(formattedTimes[i]) < single) {
-        single = formattedTimes[i];
-      }
-    }
-  }
-  if (single == 'DNF') {
-    return single;
-  } else {
-    return reformatTime(single);
-  }
-}
-
-// calculate the trimmed average of 5 given the array of times and penalties
-function calculateAverage(times, penalties) {
-  var formattedTimes = formatTimes(times, penalties);
-  var i, DNFCount = 0, minIndex, maxIndex, minValue, maxValue;
-  for (i = 0; i < formattedTimes.length; i++) {
-    if (formattedTimes[i] == 'DNF') {
-      DNFCount++;
-    }
-  }
-  if (DNFCount > 1) {
-    return 'DNF';
-  }
-  if (formattedTimes[0] != 'DNF') {
-    minIndex = 0;
-    maxIndex = 0;
-    minValue = parseFloat(formattedTimes[0]);
-    maxValue = parseFloat(formattedTimes[0]);
-  } else {
-    minIndex = 1;
-    maxIndex = 1;
-    minValue = parseFloat(formattedTimes[1]);
-    maxValue = parseFloat(formattedTimes[1]);
-  }
-  for (i = 0; i < formattedTimes.length; i++) {
-    if (formattedTimes[i] == 'DNF') {
-      maxValue = formattedTimes[i];
-      maxIndex = i;
-      break;
-    }
-    if (parseFloat(formattedTimes[i]) > maxValue) {
-      maxValue = parseFloat(formattedTimes[i]);
-      maxIndex = i;
-    }
-  }
-  for (i = 0; i < formattedTimes.length; i++) {
-    if ((i != maxIndex) && (parseFloat(formattedTimes[i]) < minValue)) {
-      minValue = parseFloat(formattedTimes[i]);
-      minIndex = i;
-    }
-  }
-  if ((minIndex === 0) && (maxIndex === 0)) {
-    maxIndex = 1;
-  }
-  var sum = 0;
-  for (i = 0; i < formattedTimes.length; i++) {
-    if ((i != minIndex) && (i != maxIndex)) {
-      sum += parseFloat(formattedTimes[i]);
-    }
-  }
-  var average = sum / (formattedTimes.length - 2);
-  return reformatTime(average);
-}
-
-// calculate FMC mean of 3
-function calculateFMCMean(moves) {
-  return ((moves[0] + moves[1] + moves[2]) / 3).toFixed(2);
-}
-
-// calculate the mean of 3 given the array of times and penalties
-function calculateMean(times, penalties) {
-  var formattedTimes = formatTimes(times, penalties);
-  var i, DNFCount = 0;
-  for (i = 0; i < formattedTimes.length; i++) {
-    if (formattedTimes[i] == 'DNF') {
-      DNFCount++;
-    }
-  }
-  if (DNFCount > 0) {
-    return 'DNF';
-  }
-  var sum = 0;
-  for (i = 0; i < formattedTimes.length; i++) {
-    sum += parseFloat(formattedTimes[i]);
-  }
-  var mean = sum / formattedTimes.length;
-  return reformatTime(mean);
-}
-
-// return an array of results in milliseconds taking penalties into account
-function formatTimes(times, penalties) {
-  var formattedTimes = [];
-  var i, unsplitTimes = [];
-  for (i = 0; i < times.length; i++) {
-    var res = times[i].split(':');
-    if (res.length > 1) {
-      unsplitTimes[i] = (parseFloat(res[0]) * 60) + parseFloat(res[1]);
-    } else {
-      unsplitTimes[i] = parseFloat(res[0]);
-    }
-    if ((penalties[i] == '(DNF)') || (unsplitTimes[i] === '')) {
-      formattedTimes[i] = 'DNF';
-    } else if (penalties[i] == '(+2)') {
-      formattedTimes[i] = parseFloat(unsplitTimes[i]) + 2;
-    } else {
-      formattedTimes[i] = unsplitTimes[i];
-    }
-  }
-  return formattedTimes;
-}
-
-// convert the time from milliseconds to minutes:seconds.milliseconds
-function reformatTime(time) {
-  if (isNaN(time)) {
-    return 'DNF';
-  }
-  if (parseFloat(time) <  60) {
-    return parseFloat(time).toFixed(2);
-  } else {
-    var min = Math.floor(parseFloat(time) / 60);
-    var sec = (parseFloat(time) % 60).toFixed(2);
-    if (sec < 10) {
-      return min + ':0' + sec;
-    } else {
-      return min + ':' + sec;
-    }
-  }
-}
