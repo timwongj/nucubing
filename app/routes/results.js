19a412386e1eef19b4b30c5364b9ed650baf5108
timwongj <wong.t@husky.neu.edu>
2015-12-05 15:12:25 -0500

    fixed result saving

--- app/routes/results.js
+++ app/routes/results.js
@@ -22,19 +22,20 @@ module.exports = (function() {
       console.log(req.user ? ('name: ' + req.user.displayName + ', facebook_id: ' + req.user.facebook_id) : 'no User');
       console.log(req.body ? ('week: ' + req.body.week + ', event: ' + req.body.event + ', status: ' + req.body.status + ', data: ' + req.body.data) : 'no body');
       if (req.user) {
-        Result.remove({'week':result.week, 'event':result.event, 'email':result.email}).exec(function(err) {
+        Result.remove({'week':req.body.week, 'event':req.body.event, 'email':req.body.email}).exec(function(err) {
           if (err) {
             res.status(500).json({'message':'cannot save result'});
           }
-          var result = new Result();
-          result.event = req.body.event;
-          result.week = req.body.week;
-          result.email = req.user.email;
-          result.firstName = req.user.firstName;
-          result.lastName = req.user.lastName;
-          result.facebook_id = req.user.facebook_id;
-          result.status = req.body.status;
-          result.data = req.body.data;
+          var result = new Result({
+            event: req.body.event,
+            week: req.body.week,
+            email: req.user.email,
+            firstName: req.user.firstName,
+            lastName: req.user.lastName,
+            facebook_id: req.user.facebook_id,
+            status: req.body.status,
+            data: req.body.data
+          });
           result.save(function(err) {
             if (err) {
               res.status(500).json({'message':'cannot save result'});
